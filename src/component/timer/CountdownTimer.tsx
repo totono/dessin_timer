08296307f3849cdd0d790aa4114573bcf30de8b7
totono <thisistotono@gmail.com>
2024-03-11 20:53:59 +0900

    added uunit of time changer

--- src/component/timer/CountdownTimer.tsx
+++ src/component/timer/CountdownTimer.tsx
@@ -3,68 +3,78 @@ import { Select } from "@kuma-ui/core"
 import { Timer } from '../hooks/useTimer';
 import './CountdownTimer.css';
 
-interface TimerProps{
+interface TimerProps {
     timer: Timer;
 }
 
-const CountdownTimer = ({ timer }:TimerProps) => {
-  const [drawerVisible, setDrawerVisible] = useState(false);
+const CountdownTimer = ({ timer }: TimerProps) => {
+    const [drawerVisible, setDrawerVisible] = useState(false);
+    const [timeUnit, setTimeUnit] = useState('minute');
+    const [timeOptions, setTimeOptions] = useState([...Array(10).keys()].map(i => i + 1));
 
+    useEffect(() => {
+        function handleMouseMove(e: MouseEvent) {
+            const { innerWidth, innerHeight } = window;
+            const { clientX, clientY } = e;
 
-  useEffect(() => {
-    function handleMouseMove(e: MouseEvent) {
-      // ウィンドウサイズを取得
-      const { innerWidth, innerHeight } = window;
-      // マウスの位置を取得
-      const { clientX, clientY } = e;
-  
-      const hoverRight30 = clientX > innerWidth * 0.8;
-  
-      const hoverTop50 = clientY < innerHeight * 0.55;
-  
-      // 条件を満たしたらドロワーを開く
-      if (hoverRight30 && hoverTop50) {
-        setDrawerVisible(true);
-      } else {
-        setDrawerVisible(false);
-      }
-    }
-    // イベントリスナーを追加
-    window.addEventListener('mousemove', handleMouseMove);
-  
-    // コンポーネントのアンマウント時にイベントリスナーを削除
-    return () => {
-      window.removeEventListener('mousemove', handleMouseMove);
+            const hoverRight30 = clientX > innerWidth * 0.8;
+            const hoverTop50 = clientY < innerHeight * 0.55;
+
+            if (hoverRight30 && hoverTop50) {
+                setDrawerVisible(true);
+            } else {
+                setDrawerVisible(false);
+            }
+        }
+
+        window.addEventListener('mousemove', handleMouseMove);
+
+        return () => {
+            window.removeEventListener('mousemove', handleMouseMove);
+        };
+    }, []);
+
+    const formatNumber = (num: number) => num.toString().padStart(2, '0');
+
+    const handleTimeUnitChange = (e: Event) => {
+        const newTimeUnit = (e.target as HTMLSelectElement).value;
+        setTimeUnit(newTimeUnit);
+
+        if (newTimeUnit === 'minute') {
+            setTimeOptions([...Array(10).keys()].map(i => i + 1));
+        } else {
+            // make step 10 seconds
+            setTimeOptions([...Array(60).keys()].map(i => i + 1).filter(i => i % 10 === 0));
+        }
     };
-  }, []);
 
+    const handleTimeChange = (e: Event) => {
+        const newTime = parseInt((e.target as HTMLSelectElement).value, 10);
+        const seconds = timeUnit === 'minute' ? newTime * 60 : newTime;
 
-  const formatNumber = (num: number) => num.toString().padStart(2, '0');
+        timer.setTotalSeconds(seconds);
+        timer.setInitialTotalSecondsState(seconds);
+        timer.setShouldResetTimer(true);
+    };
 
-
-  const handleTimeChange = (e: Event) => {
-    console.log('handleTimeChange');
-    console.log((e.target as HTMLInputElement).value)
-
-    const newMinutes = parseInt((e.target as HTMLInputElement).value, 10);
-    timer.setTotalSeconds(newMinutes * 60);
-    timer.setInitialTotalSecondsState(newMinutes * 60);
-    timer.setShouldResetTimer(true);
-  }
-
-  return (
-    <div className="countdownTimer">
-     {`${formatNumber(timer.minutes)}:${formatNumber(timer.seconds)}`}
-     {drawerVisible && (
-        <div className="timeSettingDrawer">
-          <Select onChange={handleTimeChange} >
-            {[...Array(10).keys()].map(i => (
-              <option key={i} value={i+1}>{i+1} 分</option>
-            ))}
-          </Select>
-        </div>)}
-    </div>
-  );
+    return (
+        <div className="countdownTimer">
+            {`${formatNumber(timer.minutes)}:${formatNumber(timer.seconds)}`}
+            {drawerVisible && (
+                <div className="timeSettingDrawer">
+                    <Select onChange={handleTimeChange}>
+                        {timeOptions.map(i => (
+                            <option key={i} value={i}>{i}</option>
+                        ))}
+                    </Select>
+                    <Select onChange={handleTimeUnitChange}>
+                        <option value="minute">分</option>
+                        <option value="second">秒</option>
+                    </Select>
+                </div>
+            )}
+        </div>
+    );
 };
 
 export default CountdownTimer;
